26a0f1912ec3c1c7c5ef814525505ab3a64344a1
Shmookoff <shmookoff@gmail.com>
2021-04-30 01:32:54 +0500

    fix

--- app/server.py
+++ app/server.py
@@ -5,31 +5,29 @@ import psycopg2
 import json
 from rsc.config import vk_sets, sets
 
-def create_app():
-    ipc_client = ipc.Client(secret_key = sets['ipcSecretKey'])
-    app = Quart(__name__)
-    return app
 
-    @app.route('/')
-    async def index():
-        return 'WallPost'
+ipc_client = ipc.Client(secret_key = sets['ipcSecretKey'])
+app = Quart(__name__)
 
-    @app.route('/oauth2/login')
-    async def login():
-        temp_key = f"{request.args.get('key')}"
-        return redirect(f'https://oauth.vk.com/authorize?client_id={vk_sets["appId"]}&display=page&redirect_uri={vk_sets["redirectUri"]}&scope=friends,groups,offline&response_type=code&state={temp_key}&v=5.130', code=302)
+@app.route('/')
+async def index():
+    return 'WallPost'
 
-    @app.route('/oauth2/redirect')
-    async def auth_redirect():
-        code = request.args.get('code')
-        response = requests.post(f'https://oauth.vk.com/access_token?client_id={vk_sets["appId"]}&client_secret={vk_sets["secureKey"]}&redirect_uri={vk_sets["redirectUri"]}&code={code}').json()
+@app.route('/oauth2/login')
+async def login():
+    temp_key = f"{request.args.get('key')}"
+    return redirect(f'https://oauth.vk.com/authorize?client_id={vk_sets["appId"]}&display=page&redirect_uri={vk_sets["redirectUri"]}&scope=friends,groups,offline&response_type=code&state={temp_key}&v=5.130', code=302)
 
-        response = await ipc_client.request(
-            "authentication", key = request.args.get('state'), token = response['access_token']
-        )
+@app.route('/oauth2/redirect')
+async def auth_redirect():
+    code = request.args.get('code')
+    response = requests.post(f'https://oauth.vk.com/access_token?client_id={vk_sets["appId"]}&client_secret={vk_sets["secureKey"]}&redirect_uri={vk_sets["redirectUri"]}&code={code}').json()
 
-        return response
+    response = await ipc_client.request(
+        "authentication", key = request.args.get('state'), token = response['access_token']
+    )
+
+    return response
 
 if __name__ == '__main__':
-    app = create_app()
     app.run(debug=False) 
