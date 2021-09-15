feb29f6603eb58c817405d1bf86b0f878e6f1f58
Shmookoff <shmookoff@gmail.com>
2021-09-15 09:44:34 +0500

    fix incorrect attrs reference

--- app/cogs/subs.py
+++ app/cogs/subs.py
@@ -396,7 +396,7 @@ class Subscriptions(commands.Cog):
             return
 
         async with AsyncVkExecuteRequestPool() as pool:
-            usr_REQ = pool.add_call('users.get', ctx.usr.token, self.usr_call_attrs)
+            usr_REQ = pool.add_call('users.get', ctx.usr.token, self.users_get_attrs)
         usr_RESP = usr_REQ.result
         embed = compile_wall_embed(usr_RESP[0])
 
